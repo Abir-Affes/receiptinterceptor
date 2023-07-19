7f6adb8575a5ed2dce62231b4133f1b6fbaf4fed
Abir-Affes <abir.affes@ensi-uma.tn>
2023-07-19 10:38:35 +0200

    receiptinterceptor project

--- src/test/java/tn/idrm/receiptinterceptor/IntegrationTest.java
+++ src/test/java/tn/idrm/receiptinterceptor/IntegrationTest.java
@@ -6,7 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import tn.idrm.receiptinterceptor.ReceiptinterceptordbApp;
 import tn.idrm.receiptinterceptor.config.AsyncSyncConfiguration;
 import tn.idrm.receiptinterceptor.config.EmbeddedSQL;
 
